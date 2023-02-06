1c89cfd0ffc41fadbb97bc8988cb05396bc0199c
TheDevelopersKE <thedeveloperske@gmail.com>
2023-02-06 17:12:38 +0300

    Added Sample Data

--- src/App.js
+++ src/App.js
@@ -3,25 +3,40 @@ import './App.css'
 import ContactCard from './ContactCard'
 
 const App = () => {
+
+  const contacts = [
+    {
+      name : "Jenny Bondon",
+      email : "jeny@bondon.com",
+      avatarUrl : "https://via.placeholder.com/150",
+      age : 57
+    },
+    {
+      name : "Cara Milovi",
+      email : "cara@milovi.com",
+      avatarUrl : "https://via.placeholder.com/150",  
+      age : 22
+    },
+    {
+      name : "Yorgi Koskof", 
+      email : "yogikoskof@russia.com", 
+      avatarUrl : "https://via.placeholder.com/150",  
+      age : 43,
+    }
+  ];
+
   return (
     <div>
-      <ContactCard 
-        name = "Jenny Bondon" 
-        email = "jeny@bondon.com" 
-        avatarUrl = "https://via.placeholder.com/150"  
-        age = {57}/>
-        
-      <ContactCard 
-        name = "Cara Milovi" 
-        email = "cara@milovi.com" 
-        avatarUrl = "https://via.placeholder.com/150"  
-        age = {22}/>
-
-      <ContactCard 
-        name = "Yorgi Koskof" 
-        email = "yogikoskof@russia.com" 
-        avatarUrl = "https://via.placeholder.com/150"  
-        age = {43}/>
+      {contacts.map((contact, index) => {
+        return (
+          <ContactCard 
+            key={index}
+            name = {contact.name}
+            email = {contact.email}
+            avatarUrl = {contact.avatarUrl}
+            age = {contact.age}/>
+        )
+      })}
     </div>
   )
 }
