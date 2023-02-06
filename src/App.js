8b7f5978b2972e2bcd5920690d2904249e179271
TheDevelopersKE <thedeveloperske@gmail.com>
2023-02-06 18:49:53 +0300

    Added API endpoint to pull data from https://www.randomuser.me

--- src/App.js
+++ src/App.js
@@ -1,40 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import ContactCard from './ContactCard'
 
 const App = () => {
 
-  const contacts = [
-    {
-      name : "Jenny Bondon",
-      email : "jeny@bondon.com",
-      avatarUrl : "https://via.placeholder.com/150",
-      age : 57
-    },
-    {
-      name : "Cara Milovi",
-      email : "cara@milovi.com",
-      avatarUrl : "https://via.placeholder.com/150",  
-      age : 22
-    },
-    {
-      name : "Yorgi Koskof", 
-      email : "yogikoskof@russia.com", 
-      avatarUrl : "https://via.placeholder.com/150",  
-      age : 43,
-    }
-  ];
+  // const contacts = [
+  //   {
+  //     name : "Jenny Bondon",
+  //     email : "jeny@bondon.com",
+  //     avatarUrl : "https://via.placeholder.com/150",
+  //     age : 57
+  //   },
+  //   {
+  //     name : "Cara Milovi",
+  //     email : "cara@milovi.com",
+  //     avatarUrl : "https://via.placeholder.com/150",  
+  //     age : 67
+  //   },
+  //   {
+  //     name : "Yorgi Koskof", 
+  //     email : "yogikoskof@russia.com", 
+  //     avatarUrl : "https://via.placeholder.com/150",  
+  //     age : 77,
+  //   },
+  //   {
+  //     name : "Brad Whittaker", 
+  //     email : "brad@russia.com", 
+  //     avatarUrl : "https://via.placeholder.com/150",  
+  //     age : 87,
+  //   },
+  //   {
+  //     name : "Q Bondon", 
+  //     email : "q@universalexports.com", 
+  //     avatarUrl : "https://via.placeholder.com/150",  
+  //     age : 97,
+  //   }
+  // ];
 
+  const [results, setResults] = useState([]);
+
+    useEffect(()=>{
+      fetch("https://randomuser.me/api/?results=10")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setResults(data.results)
+    });
+  }, []) 
+ 
   return (
     <div>
-      {contacts.map((contact, index) => {
+      {results.map((result, index) => {
         return (
           <ContactCard 
             key={index}
-            name = {contact.name}
-            email = {contact.email}
-            avatarUrl = {contact.avatarUrl}
-            age = {contact.age}/>
+            name = {result.name.title + " " + result.name.first + " " + result.name.last}
+            email = {result.email}
+            avatarUrl = {result.picture.large}
+            nat = {result.nat}
+            cell = {result.cell}
+            phone = {result.phone}
+            age = {result.dob.age}/>
         )
       })}
     </div>
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -5,18 +5,26 @@ const ContactCard = (props) => {
     console.log(props);
 
     const [showAge, setShowAge] = useState(false);
+    const [showPhone, setShowPhone] = useState(false);
 
   return (
     <div>
-    <div className='contact-card'>
-      <img src={props.avatarUrl} alt='profile'/>
-      <div className='user-details'>
-        <p>Name : {props.name}</p>
-        <p>Email : {props.email}</p>
-        <button onClick={() => setShowAge(!showAge)}>Show Age</button>        
-        {showAge && <p>Age : {props.age}</p> }
-      </div>
-    </div></div>
+        <div className='contact-card'>
+            <div className='row'>
+                <img src={props.avatarUrl} alt='profile'/>
+            </div>      
+            <div className='user-details'>
+                <p>Name : {props.name}</p>
+                <p>Email : {props.email}</p>
+                <p>Nationality : {props.nat}</p> 
+                <p>Cell : {props.cell}</p>
+                {showPhone && <p>Phone : {props.phone}</p> }
+                {showAge && <p>Age : {props.age}</p> }
+                <button onClick={() => setShowAge(!showAge)}>Show Age</button>        
+                <button onClick={() => setShowPhone(!showPhone)}>Show Phone</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
