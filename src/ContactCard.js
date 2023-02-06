86651367355497d622c205db165254770a07f881
TheDevelopersKE <thedeveloperske@gmail.com>
2023-02-06 16:26:17 +0300

    Added Contact Card

---
+++ src/ContactCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ContactCard = () => {
+  return (
+    <div>
+    <div className='contact-card'>
+      <img src='https://via.placeholder.com/150' alt='profile'/>
+      <div className='user-details'>
+        <p>Name : Hanson Williams</p>
+        <p>Email : thedeveloperske@gmail.com</p>
+        <p>Age : 33</p>
+      </div>
+    </div></div>
+  )
+}
+
+export default ContactCard
